804a19167527f6a9332bc23ee62161af5a693871
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-08-25 12:55:24 +0100

    function signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,7 +1,28 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
+// const logDetails = (uid: StringORNum, item: string) => {
+//     console.log(`${item} has a uid of ${uid}`);
+// }
+// const greet = (user: ObjWithName) => {
+//     console.log(`${user.name} says Hello`);
+// }
+// Function Signatures
+//Example 1...
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-const greet = (user) => {
-    console.log(`${user.name} says Hello`);
+//Example 2...
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
 };
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old!`);
+};
+logDetails({ name: 'Emmanuel', age: 27 });
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,10 +1,43 @@
 type StringORNum = string | number;
 type ObjWithName = {name: string, uid: StringORNum};
 
-const logDetails = (uid: StringORNum, item: string) => {
-    console.log(`${item} has a uid of ${uid}`);
+// const logDetails = (uid: StringORNum, item: string) => {
+//     console.log(`${item} has a uid of ${uid}`);
+// }
+
+// const greet = (user: ObjWithName) => {
+//     console.log(`${user.name} says Hello`);
+// }
+
+
+// Function Signatures
+
+//Example 1...
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`)
 }
 
-const greet = (user: ObjWithName) => {
-    console.log(`${user.name} says Hello`);
+//Example 2...
+
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
 }
+
+//Example 3...
+type person = { name: string; age: number}
+
+let logDetails: (obj: person) => void;
+
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old!`)
+}
+
+logDetails({name: 'Emmanuel', age: 27});
